d8260ec6c087b878e999b24b540a582b61673e92
Saim Rizvi <shr2mz@gmail.com>
2021-10-07 13:34:39 -0400

    commit tree d6268a570e1f7caef525709dee41c6fe7d57fe43

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+const firstName = ["Gray", "Worm", "Security", 1]
+
+const createEmployeeRecord = (firstName) => {
+    firstName = firstName[0]
+    familyNmae = firstName[1]
+    title = firstName[2]
+    payPerHour = firstName[3]
+    const timeInEvent = []
+    const timeOutEvent = []
+    return firstName
+    
+
+
+}
